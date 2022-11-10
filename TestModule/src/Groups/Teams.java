a44e1604a52bf53ee6ea6621b41817892da1692c
Alejandro López <aleespazx@gmail.com>
2022-11-10 00:42:44 +0000

    New files with new classes

---
+++ TestModule/src/Groups/Teams.java
@@ -0,0 +1,12 @@
+package Groups;
+
+public class Teams {
+    String Name;
+
+    public Teams(String Name){
+        this.Name = Name;
+    }
+    public void PrintName(){
+        System.out.println(this.Name);
+    }
+}
--- TestModule/src/Main.java
+++ TestModule/src/Main.java
@@ -1,13 +1,11 @@
+import Groups.Teams;
+
 import java.util.ArrayList;
 
 public class Main {
-
     public static void main(String[] args) {
 
-        ArrayList<String> TestArray = new ArrayList<>();
-        TestArray.add("Alejandro");
-        System.out.println(TestArray);
-
-
+        Teams Mexico = new Teams("Mexico");
+        Mexico.PrintName();
     }
 }
